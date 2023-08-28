b31143f3f1aa5d5a0540a742a9a3578833089910
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 18:12:59 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  int numFemale;
+  int numMale;
+  double percF, percM;
+
+  /* ************************************
+    Code your program here
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+  cout << "Percentage of Male: " << percF << endl;
+  cout << "Percentage of Female: " << percM << endl;
+}
